296991efdd2b1f4e9ab0c93545fa4829c0b139e0
insightdealerin-Shag <insightdealer.in@gmail.com>
2026-07-01 17:52:27 +0400

    auto update 2026-07-01 17:52:27

--- run_dashboard.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Running dashboard script...
-echo.
-python "C:\Users\Shaghaf Ahmed\OneDrive\Attachments\Desktop\Work\AUTOMATE EXCEL\QHSE_dashboard\update_dashboard.py" 2>&1
-echo.
-echo ================================
-echo Script finished! Check above for errors.
-echo ================================
-pause
